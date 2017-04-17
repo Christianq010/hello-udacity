ecf1561a161401466bc18018e4cf53639e5dd361
Christiaan Quyn <christianq010@gmail.com>
2017-04-17 16:55:18 +0530

    feat: Add Variable templating Engine (Jinja) for Shopping Cart feature

---
+++ appBackup.py
@@ -0,0 +1,124 @@
+# This is a backup of our main.py file
+
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import cgi
+
+form = """
+    <form method="post">
+        what is your birthday?
+        <br>
+
+        <label> Month
+            <input type="text" name="month" value="%(month)s">
+        </label>
+
+        <label> Day
+            <input type="text" name="day" value="%(day)s">
+        </label>
+
+        <label> Year
+            <input type="text" name="year" value="%(year)s">
+        </label>
+
+        <div>%(error)s</div>
+
+        <br>
+        <br>
+        <input type="submit">
+    </form>
+"""
+
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+
+
+# Make a mapping of the first 3 letters to lower case for Months array
+month_abbvs = dict((m[:3].lower(),m) for m in months)
+
+# Validity Check Functions
+def valid_month(month):
+    if month:
+        short_month = month[:3].lower()
+        return month_abbvs.get(short_month)
+
+def valid_day(day):
+    if day and day.isdigit():
+        day = int(day)
+        if day > 0 and day <= 31:
+            return day
+
+def valid_year(year):
+    if year and year.isdigit():
+        year = int(year)
+        if year > 1900 and year < 2020:
+            return year
+
+# Implementing HTML Escaping via Python
+def escape_html(s):
+    return cgi.escape(s, quote=True)
+
+
+class MainHandler(webapp2.RequestHandler):
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error": error,
+                                        "month": escape_html(month),
+                                        "day": escape_html(day),
+                                        "year": escape_html(year)})
+
+    def get(self):
+        # Hello
+        self.write_form()
+
+    def post(self):
+        #what the user adds into the fields
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
+
+        month = valid_month(self.request.get('month'))
+        day = valid_day(self.request.get('day'))
+        year = valid_year(self.request.get('year'))
+
+        if not (month and day and year):
+            # render error div but leave what user typed in
+            self.write_form("That doesn't look valid",
+                            user_month, user_day, user_year)
+            # else if all true
+        else:
+            self.redirect("/thanks")
+
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("Thanks! Das a Valid Form")
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/thanks', ThanksHandler)
+], debug=True)
--- main.py
+++ main.py
@@ -1,122 +1,62 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
+
 import webapp2
-import cgi
+import jinja2
 
-form = """
-    <form method="post">
-        what is your birthday?
-        <br>
+# Our templates to be stored in a templates folder
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader= jinja2.FileSystemLoader(template_dir))
 
-        <label> Month
-            <input type="text" name="month" value="%(month)s">
-        </label>
 
-        <label> Day
-            <input type="text" name="day" value="%(day)s">
-        </label>
+hidden_html = """
+<input type="hidden" name="food" value="%s">
 
-        <label> Year
-            <input type="text" name="year" value="%(year)s">
-        </label>
+"""
+item_html = "<li>%s</li>"
 
-        <div>%(error)s</div>
-
-        <br>
-        <br>
-        <input type="submit">
-    </form>
+shopping_list_html = """
+<br>
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
 """
 
-months = ['January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
-          'August',
-          'September',
-          'October',
-          'November',
-          'December']
 
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-# Make a mapping of the first 3 letters to lower case for Months array
-month_abbvs = dict((m[:3].lower(),m) for m in months)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
-# Validity Check Functions
-def valid_month(month):
-    if month:
-        short_month = month[:3].lower()
-        return month_abbvs.get(short_month)
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
-def valid_day(day):
-    if day and day.isdigit():
-        day = int(day)
-        if day > 0 and day <= 31:
-            return day
-
-def valid_year(year):
-    if year and year.isdigit():
-        year = int(year)
-        if year > 1900 and year < 2020:
-            return year
-
-# Implementing HTML Escaping via Python
-def escape_html(s):
-    return cgi.escape(s, quote=True)
-
-
-class MainHandler(webapp2.RequestHandler):
-    def write_form(self, error="", month="", day="", year=""):
-        self.response.out.write(form % {"error": error,
-                                        "month": escape_html(month),
-                                        "day": escape_html(day),
-                                        "year": escape_html(year)})
-
+class MainPage(Handler):
     def get(self):
-        # Hello
-        self.write_form()
+        self.render("shopping_list.html")
 
-    def post(self):
-        #what the user adds into the fields
-        user_month = self.request.get('month')
-        user_day = self.request.get('day')
-        user_year = self.request.get('year')
-
-        month = valid_month(self.request.get('month'))
-        day = valid_day(self.request.get('day'))
-        year = valid_year(self.request.get('year'))
-
-        if not (month and day and year):
-            # render error div but leave what user typed in
-            self.write_form("That doesn't look valid",
-                            user_month, user_day, user_year)
-            # else if all true
-        else:
-            self.redirect("/thanks")
-
-class ThanksHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write("Thanks! Das a Valid Form")
+        # output = form_html
+        # output_hidden = ""
+        #
+        # items = self.request.get_all("food")
+        # if items:
+        #     output_items = ""
+        #     for item in items:
+        #         output_hidden += hidden_html % item
+        #         output_items += item_html % item
+        #
+        #     output_shopping = shopping_list_html % output_items
+        #     output += output_shopping
+        #
+        # output = output % output_hidden
+        #
+        # self.write(output)
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/thanks', ThanksHandler)
+    ('/', MainPage),
 ], debug=True)
